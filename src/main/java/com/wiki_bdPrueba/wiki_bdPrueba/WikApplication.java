a04db5e0ec86210f3574b274176c12fc85bd91be
dell <dell@localhost.localdomain>
2020-07-08 11:17:00 -0500

    se crea proyecto

---
+++ src/main/java/com/wiki_bdPrueba/wiki_bdPrueba/WikApplication.java
@@ -0,0 +1,13 @@
+package com.wiki_bdPrueba.wiki_bdPrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikApplication.class, args);
+	}
+
+}
